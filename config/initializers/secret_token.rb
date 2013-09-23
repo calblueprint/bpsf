6872b7f0dd80d4a06fd6f69af5267a513886ab28
Wonjun Jeong <jeong.wonjun@gmail.com>
2013-09-23 16:39:21 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BPSF::Application.config.secret_token = '811f42ea12dfe4f0fbedab9c883aabe5e1938fea9c5d3fbd2a9157cb331ac417695ce6ba5e85a55a0c92fbdfc25a87fba19f30aa8a203569fdbe197cacd1c894'
