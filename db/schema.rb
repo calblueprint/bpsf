c4b0f23518168a0c218dd1646d266ff9db19d6fe
Sam Lau <samlau95@gmail.com>
2013-10-02 15:29:39 -0700

    Update schema with grants

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,15 @@
 
 ActiveRecord::Schema.define(:version => 20130930035515) do
 
+  create_table "grants", :force => true do |t|
+    t.string   "name"
+    t.string   "organization"
+    t.integer  "sum"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+    t.string   "type"
+  end
+
   create_table "user_admins", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
@@ -40,16 +49,5 @@ ActiveRecord::Schema.define(:version => 20130930035515) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-
-  create_table "grants", :force => true do |t|
-    t.string   "name"
-    t.string   "organization"
-    t.integer  "sum"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-    t.string   "type"
-  end
-
 
 end
