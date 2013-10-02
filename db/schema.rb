2332d73cb9c229219eebae8b9e5467a071720ccd
varunrau <varunrau@gmail.com>
2013-10-02 14:51:47 -0700

    added name field to users -- surprised I totally forgot about that before oh well lolz

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130930035515) do
+ActiveRecord::Schema.define(:version => 20131002215105) do
+
+  create_table "grants", :force => true do |t|
+    t.string   "name"
+    t.string   "organization"
+    t.integer  "sum"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+    t.string   "type"
+  end
 
   create_table "user_admins", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -40,16 +49,5 @@ ActiveRecord::Schema.define(:version => 20130930035515) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-
-  create_table "grants", :force => true do |t|
-    t.string   "name"
-    t.string   "organization"
-    t.integer  "sum"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-    t.string   "type"
-  end
-
 
 end
