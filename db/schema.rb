a83260d72da76103f548c37a30e69466b4ce5c72
varunrau <varunrau@gmail.com>
2013-10-02 15:22:23 -0700

    added type column for STI

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131002215738) do
+ActiveRecord::Schema.define(:version => 20131002222200) do
 
   create_table "grants", :force => true do |t|
     t.string   "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(:version => 20131002215738) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "type"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
