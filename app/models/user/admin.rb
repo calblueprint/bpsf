b12f9d208cb20c3f7fd088a2d3db81bf7808cfe6
Sam Lau <samlau95@gmail.com>
2013-10-02 15:14:31 -0700

    Annotate users

--- app/models/user/admin.rb
+++ app/models/user/admin.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: user_admins
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User::Admin < ActiveRecord::Base
   # attr_accessible :title, :body
 end
--- app/models/user/recipient.rb
+++ app/models/user/recipient.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: user_recipients
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User::Recipient < ActiveRecord::Base
   # attr_accessible :title, :body
 end
