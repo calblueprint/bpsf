af5b76ff766fe174d832e982eb01b551b5852e86
jaytlin <heyjaylin@gmail.com>
2013-10-03 13:41:49 -0700

    Remove user_admins and user_recipients tables from schema

--- db/schema.rb
+++ db/schema.rb
@@ -22,17 +22,6 @@ ActiveRecord::Schema.define(:version => 20131002223044) do
     t.string   "type"
   end
 
-
-  create_table "user_admins", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "user_recipients", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
