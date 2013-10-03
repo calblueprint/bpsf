8e471a4bee2c81a8a64c2db32286c6c91d36bbe8
Sam Lau <samlau95@gmail.com>
2013-10-03 16:00:41 -0700

    Annotate user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,9 @@
 #  last_sign_in_ip        :string(255)
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  type                   :string(255)
+#  first_name             :string(255)
+#  last_name              :string(255)
 #
 
 class User < ActiveRecord::Base
