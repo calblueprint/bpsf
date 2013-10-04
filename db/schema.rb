49167eb7c47365216cd19f71cdbbb878929fdc3f
varunrau <varunrau@gmail.com>
2013-10-03 22:33:14 -0700

    added migration to remove district field from schools -- all schools belong to the Berkeley school district

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131004050606) do
+ActiveRecord::Schema.define(:version => 20131004053038) do
 
   create_table "grants", :force => true do |t|
     t.string   "name"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(:version => 20131004050606) do
   create_table "schools", :force => true do |t|
     t.string   "name"
     t.integer  "donations_received"
-    t.string   "district"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
   end
