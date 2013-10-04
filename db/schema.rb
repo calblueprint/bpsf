84bcdd53eb9c92177db05b412f2c158975c961ca
varunrau <varunrau@gmail.com>
2013-10-03 22:43:10 -0700

    removed unnecessary table from schema

--- db/schema.rb
+++ db/schema.rb
@@ -34,16 +34,6 @@ ActiveRecord::Schema.define(:version => 20131004053038) do
     t.datetime "updated_at",         :null => false
   end
 
-  create_table "user_admins", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "user_recipients", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
