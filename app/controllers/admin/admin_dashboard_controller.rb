6216ad25f43e25d322b4b7373f48f89e006685d2
Sam Lau <samlau95@gmail.com>
2013-10-08 00:34:31 -0700

    Initial admin dashboard index

---
+++ app/controllers/admin/admin_dashboard_controller.rb
@@ -0,0 +1,5 @@
+class Admin::AdminDashboardController < ApplicationController
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 BPSF::Application.routes.draw do
+
   root to: 'pages#home'
 
   resources :grants
   devise_for :users
+
+  namespace :admin do
+    get '', to: 'admin_dashboard#index', as: '/'
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
