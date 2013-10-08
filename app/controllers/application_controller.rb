334ba6c34c258f946b94fde529aaf407a1c16ee1
Sam Lau <samlau95@gmail.com>
2013-10-08 00:33:34 -0700

    Redirect with error message when CanCan throws exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
+    redirect_to root_url, flash: { danger: exception.message }
   end
 end
