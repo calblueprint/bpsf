7e69a93b57a110ce96c73f6958a5892510a2e0a3
Sam Lau <samlau95@gmail.com>
2013-10-08 00:44:33 -0700

    Initial dashboard spec (currently failing)

--- spec/requests/admin_dashboard_spec.rb
+++ spec/requests/admin_dashboard_spec.rb
@@ -5,7 +5,7 @@ describe 'The admin dashboard' do
   subject { page }
 
   describe 'for non-logged in users' do
-    before { visit admin_dashboard_path }
+    before { visit admin_path }
 
     it { should have_error_message 'not authorized' }
   end
