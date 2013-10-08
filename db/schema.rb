5f70ba83e4c009b6a061501c60ab5308ed157e6d
varunrau <varunrau@gmail.com>
2013-10-08 13:40:56 -0700

    added migrations to replace user_id with recipient_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131007222801) do
+ActiveRecord::Schema.define(:version => 20131008203918) do
 
   create_table "grants", :force => true do |t|
     t.datetime "created_at",                            :null => false
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(:version => 20131007222801) do
     t.boolean  "pending",            :default => true
     t.boolean  "complete",           :default => false
     t.boolean  "rejected",           :default => false
-    t.integer  "user_id"
     t.text     "title"
     t.text     "summary"
     t.text     "subject_areas"
@@ -39,6 +38,7 @@ ActiveRecord::Schema.define(:version => 20131007222801) do
     t.integer  "n_collaborators"
     t.text     "collaborators"
     t.text     "comments"
+    t.integer  "recipient_id"
   end
 
   create_table "schools", :force => true do |t|
@@ -46,6 +46,16 @@ ActiveRecord::Schema.define(:version => 20131007222801) do
     t.integer  "donations_received"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
+  end
+
+  create_table "user_admins", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "user_recipients", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
@@ -61,6 +71,7 @@ ActiveRecord::Schema.define(:version => 20131007222801) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "name"
     t.string   "type"
     t.string   "first_name"
     t.string   "last_name"
