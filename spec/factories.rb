1531c70d9617977714a3cb161484211366408bbb
Sam Lau <samlau95@gmail.com>
2013-10-08 14:40:12 -0700

    Initialize factories

---
+++ spec/factories.rb
@@ -0,0 +1,24 @@
+FactoryGirl.define do
+
+  factory :user do
+    first_name 'User'
+    sequence(:last_name) { |n| "#{n}" }
+    sequence(:email) { |n| "user-#{n}@test.com" }
+    password 'foobar'
+  end
+
+  factory :recipient do
+    first_name 'Recipient'
+    sequence(:last_name) { |n| "#{n}" }
+    sequence(:email) { |n| "recipient-#{n}@test.com" }
+    password 'foobar'
+  end
+
+  factory :admin do
+    first_name 'Admin'
+    sequence(:last_name) { |n| "#{n}" }
+    sequence(:email) { |n| "admin-#{n}@test.com" }
+    password 'foobar'
+  end
+
+end
