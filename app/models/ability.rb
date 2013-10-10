3f493e8ebdd3f3edce972f3888d84ee1b0c887a6
Sam Lau <samlau95@gmail.com>
2013-10-10 10:53:38 -0700

    Refactor ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,17 +3,13 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    alias_action :create, :read, :update, :destroy, :to => :crud
 
     if user.type == "Admin"
       can :manage, :all
       cannot :destroy, Admin
     elsif user.type == "Recipient"
-      can :crud, Grant
-      can :update, Grant do |grant|
-        grant && grant.user_id = user.id
-      end
-      can :destroy, Grant, user_id: user.id
+      can [:create, :read], Grant
+      can [:update, :destroy], Grant, user_id = user.id
       can :read, Recipient
     else
       can :read, Grant
