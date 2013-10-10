819b9d15e6e045de8fbc168686a4d367975e04b4
jaytlin <heyjaylin@gmail.com>
2013-10-10 12:25:25 -0700

    Deny authorization to teacher dashboard to admin and public

--- app/controllers/recipient/dashboard_controller.rb
+++ app/controllers/recipient/dashboard_controller.rb
@@ -1,6 +1,11 @@
 class Recipient::DashboardController < ApplicationController
   
   def index
-    @grants = current_user.grants
+    if current_user.type == 'Recipient'
+      @grants = current_user.grants
+    elsif
+      raise CanCan::AccessDenied.new("You are not authorized to access this page.", :index, Recipient) ;
+      redirect_to root_url
+    end
   end
 end
