42601175932a7d1c9be75bf14e2f96f9c373b526
Sam Lau <samlau95@gmail.com>
2013-10-10 10:54:57 -0700

    Add toggle complete action

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,4 +4,20 @@ class Admin::DashboardController < ApplicationController
   def index
     @grants = Grant.all
   end
+
+  def toggle_complete
+    @grant = Grant.find params[:id]
+    if @grant.complete?
+      @grant.crowdfunding = false
+      @grant.crowdfund_pending = false
+      @grant.pending = false
+      @grant.rejected = false
+      @grant.save!
+    else
+      @grant.pending = true
+      @grant.save!
+    end
+    @grant.toggle! :complete
+    redirect_to admin_dashboard_path
+  end
 end
