66c0097eb07a41e2375ac0a5a5ff867c8e66e336
jaytlin <heyjaylin@gmail.com>
2013-10-11 16:42:42 -0700

    Revert "Fix pagination of grants to show titles"
    This reverts commit 5d72efbfdf7400aab20de43fc436229d6975140c.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @grants = Grant.where(:crowdfunding=>true).paginate :page => params[:page], :per_page => 3
+    # @grants = Grant.all
+    @grants = Grant.where(:crowdfunding => true).paginate(:page => params[:page], :per_page => 6)
   end
 end
