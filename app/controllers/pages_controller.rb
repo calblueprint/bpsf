0b9f25f97086b55ea2f33a40d112d9b58a609471
jaytlin <heyjaylin@gmail.com>
2013-10-10 22:20:33 -0700

    Paginate grants on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @grants = Grant.all
+    # @grants = Grant.all
+    @grants = Grant.where(:crowdfunding => true).paginate(:page => params[:page], :per_page => 6)
   end
 end
