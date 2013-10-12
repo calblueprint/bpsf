40f874894300e41b1e94660530d9b3bb232adf0b
jaytlin <heyjaylin@gmail.com>
2013-10-12 12:39:18 -0700

    Fix pagination to show grant titles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def home
-    # @grants = Grant.all
     @grants = Grant.where(:crowdfunding => true).paginate(:page => params[:page], :per_page => 6)
   end
 end
