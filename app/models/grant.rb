a4fee293c9b3eb740c8c5106e4aa8e8e81bf968a
Sam Lau <samlau95@gmail.com>
2013-10-13 02:47:31 -0700

    Delete extra schema info

--- app/models/grant.rb
+++ app/models/grant.rb
@@ -30,38 +30,6 @@
 #  recipient_id       :integer
 #
 
-# == Schema Information
-#
-# Table name: grants
-#
-#  id                 :integer          not null, primary key
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
-#  crowdfunding       :boolean          default(FALSE)
-#  crowdfund_pending  :boolean          default(FALSE)
-#  pending            :boolean          default(TRUE)
-#  complete           :boolean          default(FALSE)
-#  rejected           :boolean          default(FALSE)
-#  user_id            :integer
-#  title              :text
-#  summary            :text
- # subject_areas      :text
- # grade_level        :text
- # duration           :text
- # num_classes        :integer
- # num_students       :integer
- # total_budget       :integer
- # requested_funds    :integer
- # funds_will_pay_for :text
- # budget_desc        :text
- # purpose            :text
- # methods            :text
- # background         :text
- # n_collaborators    :integer
- # collaborators      :text
- # comments           :text
-#
-
 class Grant < ActiveRecord::Base
   attr_accessible :title, :organization, :sum, :summary, :subject_areas,
                   :grade_level, :duration, :num_classes, :num_students, :total_budget,
