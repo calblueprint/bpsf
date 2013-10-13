3ce7c8a6e440198bdbf78089471e97f9eeb9fd04
Sam Lau <samlau95@gmail.com>
2013-10-12 23:21:28 -0700

    Redirect to dashboard after log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, flash: { danger: exception.message }
   end
+
+  def after_sign_in_path_for(resource) 
+    return admin_dashboard_path     if resource.is_a?(Admin)
+    return recipient_dashboard_path if resource.is_a?(Recipient)
+  end
 end
