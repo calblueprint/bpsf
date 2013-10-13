b3cdd6c5d4e1e9abab7cec36eb5a0331b615703f
Sam Lau <samlau95@gmail.com>
2013-10-13 02:42:20 -0700

    Annotate grant

--- app/models/grant.rb
+++ app/models/grant.rb
@@ -10,7 +10,6 @@
 #  pending            :boolean          default(TRUE)
 #  complete           :boolean          default(FALSE)
 #  rejected           :boolean          default(FALSE)
-#  user_id            :integer
 #  title              :text
 #  summary            :text
 #  subject_areas      :text
@@ -28,6 +27,39 @@
 #  n_collaborators    :integer
 #  collaborators      :text
 #  comments           :text
+#  recipient_id       :integer
+#
+
+# == Schema Information
+#
+# Table name: grants
+#
+#  id                 :integer          not null, primary key
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  crowdfunding       :boolean          default(FALSE)
+#  crowdfund_pending  :boolean          default(FALSE)
+#  pending            :boolean          default(TRUE)
+#  complete           :boolean          default(FALSE)
+#  rejected           :boolean          default(FALSE)
+#  user_id            :integer
+#  title              :text
+#  summary            :text
+ # subject_areas      :text
+ # grade_level        :text
+ # duration           :text
+ # num_classes        :integer
+ # num_students       :integer
+ # total_budget       :integer
+ # requested_funds    :integer
+ # funds_will_pay_for :text
+ # budget_desc        :text
+ # purpose            :text
+ # methods            :text
+ # background         :text
+ # n_collaborators    :integer
+ # collaborators      :text
+ # comments           :text
 #
 
 class Grant < ActiveRecord::Base
