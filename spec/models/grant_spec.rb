158cd301a53d7474656bece18d9219c14c0e8ff5
varunrau <varunrau@gmail.com>
2013-10-13 16:16:07 -0700

    annotated grant model

--- spec/models/grant_spec.rb
+++ spec/models/grant_spec.rb
@@ -1,3 +1,31 @@
+# == Schema Information
+#
+# Table name: grants
+#
+#  id                 :integer          not null, primary key
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  title              :text
+#  summary            :text
+#  subject_areas      :text
+#  grade_level        :text
+#  duration           :text
+#  num_classes        :integer
+#  num_students       :integer
+#  total_budget       :integer
+#  requested_funds    :integer
+#  funds_will_pay_for :text
+#  budget_desc        :text
+#  purpose            :text
+#  methods            :text
+#  background         :text
+#  n_collaborators    :integer
+#  collaborators      :text
+#  comments           :text
+#  recipient_id       :integer
+#  state              :string(255)
+#
+
 require "spec_helper"
 
 describe Grant do
