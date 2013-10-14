1649c70a267760d09c9a077b90a98e812fb31e6d
Sam Lau <samlau95@gmail.com>
2013-10-13 18:13:12 -0700

    new and create methods implements

---
+++ app/controllers/draft_grants_controller.rb
@@ -0,0 +1,26 @@
+class DraftGrantsController < ApplicationController
+  load_and_authorize_resource
+  
+  def new
+    @draft_grant = current_user.draft_grants.build
+  end
+
+  def create
+    @draft_grant = current_user.draft_grants.build params[:draft_grant]
+    if @draft_grant.save
+      flash[:success] = 'Grant initialized!'
+      redirect_to recipient_dashboard_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+end
