a6dd0fad619492f4cb85180813d6184db8cb567c
Sam Lau <samlau95@gmail.com>
2013-10-22 20:48:53 -0700

    Annotate models

--- app/models/crowdfund.rb
+++ app/models/crowdfund.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: crowdfunds
+#
+#  id            :integer          not null, primary key
+#  deadline      :datetime
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  goal          :integer
+#  pledged_total :integer
+#
+
 class Crowdfund < ActiveRecord::Base
   attr_accessible :deadline, :goal, :pledged_total
 end
