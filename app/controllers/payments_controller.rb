dca8e488d8394c7bc74687fce46814b0db9bef32
varunrau <varunrau@gmail.com>
2013-10-24 20:51:39 -0700

    add info to payment creation

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,7 +5,9 @@ class PaymentsController < ApplicationController
       @grant = Grant.find(params[:grant_id])
       deny_access(url: url_for(@grant))
     else
-      @payment = current_user.payments.build(:amount => params[:amount])
+      @payment = current_user.payments.build(:amount => params[:amount],
+                                             :user_id => current_user.id,
+                                             :crowdfund_id => params[:grant_id])
       @payment.save
       current_user.stripe_token ||= params[:stripe_token]
       current_user.save
