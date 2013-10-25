16686be27bf8255cae17b8edac6bcdb88c955658
varunrau <varunrau@gmail.com>
2013-10-24 20:39:30 -0700

    fixed user not logged in when donating

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,8 @@
 
 module SessionsHelper
 
-  def deny_access
-    store_location
+  def deny_access(options)
+    store_location(options)
     redirect_to new_user_session_path
   end
 
@@ -12,8 +12,12 @@ module SessionsHelper
 
   private
 
-  def store_location
-    session[:return_to] = request.fullpath
+  def store_location(options)
+    if options[:url]
+      session[:return_to] = options[:url]
+    else
+      session[:return_to] = request.fullpath
+    end
   end
 
   def clear_stored_location
