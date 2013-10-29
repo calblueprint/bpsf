9a790e2e897dba4115acb3eaa97901cb692b5bd5
Sam Lau <samlau95@gmail.com>
2013-10-29 01:12:39 -0700

    Refactor payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,30 +1,27 @@
 class PaymentsController < ApplicationController
 
   def create
-    @grant = Grant.find(params[:grant_id])
+    @grant = Grant.find params[:grant_id]
     if !anyone_signed_in?
-      deny_access(url: url_for(@grant))
+      deny_access url: url_for(@grant)
     else
-      puts params[:amount]
-      @payment = current_user.payments.build(:amount => params[:amount])
-      @payment.user_id = current_user.id
-      @payment.crowdfund_id = @grant.id
-      @payment.save
+      @payment = current_user.payments.create! amount: params[:amount],
+                                               user_id: current_user.id,
+                                               crowdfund_id: @grant.id
       unless current_user.stripe_token
-        customer = Stripe::Customer.create(:email => current_user.email,
-                                         :card => params[:stripe_token],
-                                         :description => "Donor")
+        customer = Stripe::Customer.create! email: current_user.email,
+                                            card: params[:stripe_token],
+                                            description: "Donor"
         current_user.stripe_token = customer.id
-        current_user.save
+        current_user.save!
       end
       redirect_to root_url, notice: "Donation processed!"
     end
   end
 
   def destroy
-    Payment.find(params[:id]).destroy
+    Payment.destroy params[:id]
     flash[:success] = "Payment cancelled."
     redirect_to root_url
   end
-
 end
