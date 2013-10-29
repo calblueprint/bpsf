46dea24371dafd54b34d27035abb3f4e7e2b199d
Sam Lau <samlau95@gmail.com>
2013-10-29 01:09:28 -0700

    Annotate

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: payments
+#
+#  id           :integer          not null, primary key
+#  amount       :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :integer
+#  crowdfund_id :integer
+#  charge_id    :string(255)
+#
+
 class Payment < ActiveRecord::Base
   attr_accessible :amount
   belongs_to :user
