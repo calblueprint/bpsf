bea40724a1483907ed3f1a34fb9e42d6976134f6
Sam Lau <samlau95@gmail.com>
2013-11-02 21:33:29 -0700

    Better seed data to pass validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,51 +7,101 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Create Teachers (we refer to them as Recipients)
-teacher1 = Recipient.create! first_name: 'Teacher_1',
-                             last_name: 'Dev',
-                             email: 'bpsfteacher1@gmail.com',
-                             password: 'password',
-                             password_confirmation: 'password'
+def make_users
+  teacher1 = Recipient.create! first_name: 'Teacher_1',
+                               last_name: 'Dev',
+                               email: 'bpsfteacher1@gmail.com',
+                               password: 'password',
+                               password_confirmation: 'password'
 
-teacher2 = Recipient.create! first_name: 'Teacher_2',
-                             last_name: 'Dev',
-                             email: 'bpsfteacher2@gmail.com',
-                             password: 'password',
-                             password_confirmation: 'password'
+  teacher2 = Recipient.create! first_name: 'Teacher_2',
+                               last_name: 'Dev',
+                               email: 'bpsfteacher2@gmail.com',
+                               password: 'password',
+                               password_confirmation: 'password'
 
-# Create Admins
-admin1 = Admin.create! first_name: 'Admin_1',
-                       last_name: 'Dev',
-                       email: 'bpsfadmin1@gmail.com',
-                       password: 'password',
-                       password_confirmation: 'password'
+  # Create Admins
+  admin1 = Admin.create! first_name: 'Admin_1',
+                         last_name: 'Dev',
+                         email: 'bpsfadmin1@gmail.com',
+                         password: 'password',
+                         password_confirmation: 'password'
 
-admin2 = Admin.create! first_name: 'Admin_2',
-                       last_name: 'Dev',
-                       email: 'bpsfadmin2@gmail.com',
-                       password: 'password',
-                       password_confirmation: 'password'
+  admin2 = Admin.create! first_name: 'Admin_2',
+                         last_name: 'Dev',
+                         email: 'bpsfadmin2@gmail.com',
+                         password: 'password',
+                         password_confirmation: 'password'
 
-# Create Parents (they are general Users)
-parent1 = User.create! first_name: 'Parent_1',
-                       last_name: 'Dev',
-                       email: 'bpsfparent1@gmail.com',
-                       password: 'password',
-                       password_confirmation: 'password'
+  # Create Parents (they are general Users)
+  parent1 = User.create! first_name: 'Parent_1',
+                         last_name: 'Dev',
+                         email: 'bpsfparent1@gmail.com',
+                         password: 'password',
+                         password_confirmation: 'password'
 
-parent2 = User.create! first_name: 'Parent_2',
-                       last_name: 'Dev',
-                       email: 'bpsfparent2@gmail.com',
-                       password: 'password',
-                       password_confirmation: 'password'
+  parent2 = User.create! first_name: 'Parent_2',
+                         last_name: 'Dev',
+                         email: 'bpsfparent2@gmail.com',
+                         password: 'password',
+                         password_confirmation: 'password'
+end
 
-teacher1.grants.create! title: "Grant 1"
-teacher2.grants.create! title: "Grant 2"
-crowdfunding_grants = []
-crowdfunding_grants << teacher1.grants.create!(title: "Crowdfunding 1")
-crowdfunding_grants << teacher2.grants.create!(title: "Crowdfunding 2")
-crowdfunding_grants << teacher1.grants.create!(title: "Crowdfunding 3")
-crowdfunding_grants << teacher2.grants.create!(title: "Crowdfunding 4")
-crowdfunding_grants << teacher1.grants.create!(title: "Crowdfunding 5")
-crowdfunding_grants << teacher2.grants.create!(title: "Crowdfunding 6")
-crowdfunding_grants.map { |grant| grant.crowdfund }
+def make_schools
+  School.create! name: 'Berkeley High School'
+  School.create! name: 'Washington Elementary School'
+  School.create! name: 'Maybeck High School'
+end
+
+def make_grants
+  t1 = Recipient.find_by_first_name 'Teacher_1'
+  t2 = Recipient.find_by_first_name 'Teacher_2'
+  3.times do |n|
+    t1.draft_grants.create! title: "Draft #{n}",
+                            summary: Faker::Lorem.sentence
+    t2.draft_grants.create! title: "Draft #{n + 3}",
+                            summary: Faker::Lorem.sentence
+  end
+  crowdfunding_grants = []
+  3.times do |n|
+    crowdfunding_grants << t1.grants.build(title: "Grant #{n}",
+                                           summary: Faker::Lorem.sentence,
+                                           subject_areas: 'Art Science',
+                                           grade_level: "#{n + 2}",
+                                           duration: "#{n} weeks",
+                                           num_classes: n,
+                                           num_students: n * 10,
+                                           total_budget: n * 300,
+                                           requested_funds: n * 250,
+                                           funds_will_pay_for: Faker::Lorem.paragraph,
+                                           budget_desc: Faker::Lorem.paragraph,
+                                           purpose: Faker::Lorem.paragraph,
+                                           methods: Faker::Lorem.paragraph,
+                                           background: Faker::Lorem.paragraph,
+                                           n_collaborators: n,
+                                           collaborators: Faker::Lorem.paragraph,
+                                           comments: Faker::Lorem.paragraph)
+    crowdfunding_grants << t2.grants.build(title: "Grant #{n + 3}",
+                                           summary: Faker::Lorem.sentence,
+                                           subject_areas: 'Art Science',
+                                           grade_level: "#{n + 2}",
+                                           duration: "#{n} weeks",
+                                           num_classes: n,
+                                           num_students: n * 10,
+                                           total_budget: n * 300,
+                                           requested_funds: n * 250,
+                                           funds_will_pay_for: Faker::Lorem.paragraph,
+                                           budget_desc: Faker::Lorem.paragraph,
+                                           purpose: Faker::Lorem.paragraph,
+                                           methods: Faker::Lorem.paragraph,
+                                           background: Faker::Lorem.paragraph,
+                                           n_collaborators: n,
+                                           collaborators: Faker::Lorem.paragraph,
+                                           comments: Faker::Lorem.paragraph)
+  end
+  crowdfunding_grants.map { |grant| grant.crowdfund ; grant.save! }
+end
+
+make_users
+make_schools
+make_grants
