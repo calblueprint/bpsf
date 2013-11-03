3d63e6d2e00e4d62f47b2521a6274939bb97b309
vivekraghuram <VV9295@YAHOO.COM>
2013-11-03 09:39:25 -0800

    Fully implemented navbar with login and reordered javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,6 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//= require modernizr
-//= require jquery
 //= require_tree .
 //= require jquery_ujs
 
