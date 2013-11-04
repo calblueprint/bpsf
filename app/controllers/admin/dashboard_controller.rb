4eec81395c5e1a1abea13110b6941f6654f3af12
Sam Lau <samlau95@gmail.com>
2013-11-04 11:50:44 -0800

    Response with js for ajax

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -7,10 +7,12 @@ class Admin::DashboardController < ApplicationController
     @grants = Grant.all.sort
   end
 
-  # One day, this too will be AJAX
   def grant_event
     @grant = Grant.find params[:id]
     @grant.send params[:state]
-    redirect_to admin_dashboard_path
+    respond_to do |format|
+      format.html { redirect_to admin_dashboard_path }
+      format.js
+    end
   end
 end
