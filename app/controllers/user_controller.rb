cfce50eb622a6337d5aaf4603a02073100b264bd
varunrau <varunrau@gmail.com>
2013-11-06 12:05:31 -0800

    added show controller action and partials

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,18 +3,19 @@ class UserController < ApplicationController
 
   def show
     @user = User.find params[:id]
-    pledges = Payment.find_all_by_user_id params[:id]
-    @pledges = Array.new
-    pledges.each do |pledge|
-      pledge_hash = Hash.new
-      pledge_hash[pledge] = Grant.find pledge.crowdfund_id
-      @pledges.push pledge_hash
+    total_payments = Payment.find_all_by_user_id params[:id]
+    @payments = Array.new
+    total_payments.each do |payment|
+      payment_hash = Hash.new
+      payment_hash[:crowdfund] = Crowdfund.find payment.crowdfund_id
+      payment_hash[:recipient] = Recipient.find payment.recipient_id
+      payment_hash[:amount] = payment.amount
     end
   end
 
   def edit
     @user = User.find params[:id]
-    @pledges = Payment.find_by_user_id params[:id]
+    @payments = Payment.find_by_user_id params[:id]
   end
 
   def update
