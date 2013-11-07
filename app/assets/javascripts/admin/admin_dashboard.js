c80d368883c49086b88ec7445609346fba8ce3ef
varunrau <varunrau@gmail.com>
2013-11-07 01:24:30 -0800

    added unobtrusive javascript that handles ajax response

--- app/assets/javascripts/admin/admin_dashboard.js
+++ app/assets/javascripts/admin/admin_dashboard.js
@@ -1,5 +1,30 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+$(document).ready(function(){
+
+	// $(".crowdfund").click(function(event){
+	// 	alert("button pressed");
+	// 	$.ajax({
+	// 		url: crowdfund_form,
+	// 		type: "GET",
+	// 		data: {id : event.target.id },
+	// 		success: function(data) {
+	// 			alert("got something back")
+	// 			$("#modal-content").html(data);
+	// 			$("#crowdfund-modal").addClass("active");
+	// 		},
+	// 		error: function() {
+	// 			   alert("CRAP");
+	// 		}
+	// 	});
+	// });
+	$(".crowdfund").bind("ajax:success", function(evt, data, status, xhr){
+		alert("button pressed");
+		$("#modal-content").html(data);
+		$("#crowdfund-modal").addClass("active");
+	}).bind("ajax:error", function(evt, data, status, xhr){
+		// We should do something with the errors.
+		// When we add an error div this should add stuff to it.
+	});
+});
 
 
