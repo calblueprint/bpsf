71215fcd325b1187e154c5e6fe2f265c706385c7
varunrau <varunrau@gmail.com>
2013-11-08 22:14:15 -0800

    removed debug statements

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -9,7 +9,6 @@ class Admin::DashboardController < ApplicationController
 
   def grant_event
     @grant = Grant.find params[:id]
-    puts params[:state]
     @grant.send params[:state]
     respond_to do |format|
       format.html { redirect_to admin_dashboard_path }
@@ -18,7 +17,6 @@ class Admin::DashboardController < ApplicationController
   end
 
   def crowdfund_form
-    puts "CALLED CROWD FUND ON SERVER"
     @grant = Grant.find params[:id]
     @grant.state = "crowdfunding"
     @grant.save
