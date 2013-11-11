7efe6e61c79808a86ea8e0ab525dfaaab463aad8
varunrau <varunrau@gmail.com>
2013-11-11 15:10:16 -0800

    prettier formatting

--- app/controllers/crowdfund_controller.rb
+++ app/controllers/crowdfund_controller.rb
@@ -3,8 +3,8 @@ class CrowdfundController < ApplicationController
     date = params[:date]
     time = Time.new date[:year], date[:month], date[:day]
     c = Crowdfund.create deadline: time,
-                     goal: params[:goal],
-                     pledged_total: 0
+                         goal: params[:goal],
+                         pledged_total: 0
     c.grant_id = params[:grant_id]
     if c.save
       flash[:success] = "Crowfund started!"
