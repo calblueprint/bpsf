ec98c4b589a94b86a21d06a9b2cca237b8382d35
varunrau <varunrau@gmail.com>
2013-11-12 14:28:10 -0800

    add instance var crowdfund

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,6 +2,7 @@ class PaymentsController < ApplicationController
 
   def create
     @grant = Grant.find params[:grant_id]
+    @crowdfund = Crowdfund.find params[:crowdfund_id]
     if !anyone_signed_in?
       deny_access url: url_for(@grant)
     else
