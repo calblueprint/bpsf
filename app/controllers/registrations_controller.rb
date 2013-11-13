7f21a30bccf3f5d24f1c35960a0a175fd24ae43e
varunrau <varunrau@gmail.com>
2013-11-12 23:47:09 -0800

    editted redirect for recipient

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -29,7 +29,8 @@ class RegistrationsController < Devise::RegistrationsController
 
   def after_sign_up_path_for(resource)
     if resource.is_a?(Recipient)
-
+      resource.recipeint_profile = RecipientProfile.new
+      redirect_to resource
     else
       super
     end
