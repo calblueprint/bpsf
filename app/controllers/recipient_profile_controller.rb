26567e2797c1c00773f2f257b80d354d75482316
varunrau <varunrau@gmail.com>
2013-11-13 22:00:37 -0800

    added recipient controller

--- app/controllers/recipient_profile_controller.rb
+++ app/controllers/recipient_profile_controller.rb
@@ -1,7 +1,26 @@
 class RecipientProfileController < ApplicationController
-  def update
+
+  def new
+    @profile = RecipientProfile.new
   end
 
-  def edit
+  def create
+    @profile = current_user.recipient_profile.build params[:recipient_profile]
+    if @profile.save
+      flash[:success] = "Profile Created!"
+      redirect_to root_url
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @profile = RecipientProfile.find params[:id]
+    if @profile.update_attributes params[:recipient_profile]
+      flash[:success] = "Profile updated!"
+      redirect_to root_url
+    else
+      render "edit"
+    end
   end
 end
