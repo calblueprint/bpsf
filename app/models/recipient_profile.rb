a308255a7abd89bf6a4cabc360309fd95770f988
varunrau <varunrau@gmail.com>
2013-11-13 22:04:46 -0800

    added mode for profiles for teachers

--- app/models/recipient_profile.rb
+++ app/models/recipient_profile.rb
@@ -1,4 +1,4 @@
 class RecipientProfile < ActiveRecord::Base
-  attr_accessible :about, :image_url, :school_id
+  attr_accessible :about, :image_url, :school_id, :recipient_id
   belongs_to :recipient
 end
