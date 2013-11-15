67f483cbd0e06517836dbaa4ae6ab463ed1df7da
varunrau <varunrau@gmail.com>
2013-11-14 21:02:07 -0800

    added fields to recipient profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131113063825) do
+ActiveRecord::Schema.define(:version => 20131115041136) do
 
   create_table "crowdfunds", :force => true do |t|
     t.datetime "deadline"
@@ -111,9 +111,12 @@ ActiveRecord::Schema.define(:version => 20131113063825) do
     t.integer  "school_id"
     t.string   "image_url"
     t.text     "about"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
     t.integer  "recipient_id"
+    t.datetime "started_teaching"
+    t.string   "subject"
+    t.string   "grade"
   end
 
   create_table "schools", :force => true do |t|
