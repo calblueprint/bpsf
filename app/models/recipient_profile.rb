f50e687a4159b326543aecff352f7a400772246d
varunrau <varunrau@gmail.com>
2013-11-16 13:16:13 -0800

    added recipient profile fields

--- app/models/recipient_profile.rb
+++ app/models/recipient_profile.rb
@@ -1,4 +1,8 @@
 class RecipientProfile < ActiveRecord::Base
-  attr_accessible :about, :image_url, :school_id, :recipient_id, :grade, :subject
+  attr_accessible :about, :image_url, :school_id, :recipient_id, :grade, :subject, :started_teaching
   belongs_to :recipient
+
+  def years_teaching
+    Date.today.year - self.started_teaching.year
+  end
 end
