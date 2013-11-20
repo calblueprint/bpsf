2f118aec3139130a432dced0933170caed387d13
Sam Lau <samlau95@gmail.com>
2013-11-19 18:09:50 -0800

    Annotate models

--- app/models/admin_profile.rb
+++ app/models/admin_profile.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: admin_profiles
+#
+#  id            :integer          not null, primary key
+#  about         :string(255)
+#  position      :string(255)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  admin_id      :integer
+#  super_user_id :integer
+#
+
 class AdminProfile < ActiveRecord::Base
   attr_accessible :about, :position, :admin_id
   belongs_to :admin
