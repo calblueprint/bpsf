2d8e6e0e7920018b04da3bb873cda34a8940b4e9
Sam Lau <samlau95@gmail.com>
2013-11-23 14:25:38 -0800

    Refactor

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,12 @@ class PagesController < ApplicationController
   MENU = ['All', 'Art & Music', 'Supplies', 'Reading', 'Science & Math', 'Field Trips', 'Other']
 
   def home
-    if (params[:subject] && params[:subject] != 'All')
-      @grants = Grant.crowdfunding_grants.select { |grant| grant.subject_areas.include?(params[:subject])}
+    if params[:subject] && params[:subject] != 'All'
+      @grants = Grant.crowdfunding_grants.select { |grant| grant.subject_areas.include? params[:subject] }
     else
       @grants = Grant.crowdfunding_grants
     end
-    @grants = @grants.paginate(:page => params[:page], :per_page => 6)
+    @grants = @grants.paginate :page => params[:page], :per_page => 6
   end
 
   def search
@@ -16,20 +16,20 @@ class PagesController < ApplicationController
 
   def donors
     @user = current_user
-    if @user.is_a?(Admin) || @user.is_a?(SuperUser)
+    if admin_user? @user
       @users = User.all
     else
-      raise CanCan::AccessDenied.new("You are not authorized to access this page.", :donors, Pages)
+      raise CanCan::AccessDenied.new "You are not authorized to access this page.", :donors, Pages
       redirect_to :back
     end
   end
 
   def recipients
     @user = current_user
-    if @user.is_a?(Admin) || @user.is_a?(SuperUser)
+    if admin_user? @user
       @recipients = Recipient.all
     else
-      raise CanCan::AccessDenied.new("You are not authorized to access this page.", :donors, Pages)
+      raise CanCan::AccessDenied.new "You are not authorized to access this page.", :donors, Pages
       redirect_to :back
     end
   end
