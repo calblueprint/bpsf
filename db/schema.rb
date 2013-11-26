b1ce62ef75f4632ef773eb050b20dc1c78378e9b
varunrau <varunrau@gmail.com>
2013-11-25 17:25:59 -0800

    removed schema from another branch

--- db/schema.rb
+++ db/schema.rb
@@ -145,15 +145,6 @@ ActiveRecord::Schema.define(:version => 20131125213657) do
     t.datetime "updated_at",         :null => false
   end
 
-  create_table "simple_captcha_data", :force => true do |t|
-    t.string   "key",        :limit => 40
-    t.string   "value",      :limit => 6
-    t.datetime "created_at",               :null => false
-    t.datetime "updated_at",               :null => false
-  end
-
-  add_index "simple_captcha_data", ["key"], :name => "idx_key"
-
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
