4c29be5bb29f1ceb2ec3da859953c3952193e0c7
Sam Lau <samlau95@gmail.com>
2013-11-26 22:18:13 -0800

    Add note for unused method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, flash: { danger: exception.message }
   end
 
+  # This code never gets executed.
   def after_sign_in_path_for(resource)
     store_location = session[:return_to]
     clear_stored_location
