f8a7a58565668101013ccc134f30f6b61e97be5a
Sam Lau <samlau95@gmail.com>
2013-12-03 20:00:33 -0800

    Delete action for preapproved grants

--- app/controllers/preapproved_grants_controller.rb
+++ app/controllers/preapproved_grants_controller.rb
@@ -8,4 +8,10 @@ class PreapprovedGrantsController < ApplicationController
     @grant.clone_into_draft_for! current_user.id
     redirect_to recipient_dashboard_path
   end
+
+  def destroy
+    PreapprovedGrant.destroy params[:id]
+    flash[:success] = 'Preapproved grant deleted.'
+    redirect_to admin_dashboard_path
+  end
 end
