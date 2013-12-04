d695f33df817e7a7a5b444f0f2bbfe50161b6b1c
Varun Rau <varunrau@gmail.com>
2013-12-03 16:18:44 -0800

    added recipient dashboard approval

--- app/controllers/recipient/dashboard_controller.rb
+++ app/controllers/recipient/dashboard_controller.rb
@@ -1,12 +1,14 @@
 class Recipient::DashboardController < ApplicationController
-
   def index
-    if recipient?
+    if recipient? && current_user.approved
       @draft_grants = current_user.draft_grants
       @grants = current_user.grants
     elsif
-      raise CanCan::AccessDenied.new("You are not authorized to access this page.", :index, Recipient)
-      redirect_to root_url
+      if recipient? && !current_user.approved
+        raise CanCan::AccessDenied.new("Your account is pending administrator approval!", :index, Recipient)
+      else
+        raise CanCan::AccessDenied.new("You are not authorized to access this page.", :index, Recipient)
+      end
     end
   end
 end
