a9c9a49d241ff21e9a643af92811a634d939f39e
Varun Rau <varunrau@gmail.com>
2013-12-03 16:16:57 -0800

    added message admin approval

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, flash: { danger: exception.message }
+    if current_user.approved
+      redirect_to root_url, flash: { danger: exception.message }
+    else
+      redirect_to root_url, flash: { danger: "Your account is pending administrator approval!" }
+    end
   end
 
   def after_sign_in_path_for(resource)
