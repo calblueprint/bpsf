41b3182c752125bfd687c3d517b7efd739619f9b
Sam Lau <samlau95@gmail.com>
2013-12-03 20:42:34 -0800

    Remove duplicate method call

--- app/controllers/crowdfund_controller.rb
+++ app/controllers/crowdfund_controller.rb
@@ -3,15 +3,15 @@ class CrowdfundController < ApplicationController
   def create
     # Do we need the date/time variable? They don't look
     # like they're being used.
-    date = params[:date]
+    date = params[:date] ; id = params[:id]
     time = Time.new date[:year], date[:month], date[:day]
     c = Crowdfund.create deadline: time,
                          goal: params[:goal],
                          pledged_total: 0,
-                         grant_id: params[:grant_id]
+                         grant_id: id
     flash[:success] = "Crowdfund started!" if c.save
     # Why do we allow this code to run if the crowdfund didn't save?
-    @grant = Grant.find params[:grant_id]
+    @grant = Grant.find id
     @grant.crowdfund
     @grant.save
     redirect_to @grant
