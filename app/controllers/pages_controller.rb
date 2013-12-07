406fbba8c76a0e9ba214650ceb7db29ac63a16d5
Sam Lau <samlau95@gmail.com>
2013-12-06 18:29:22 -0800

    Avoid N+1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,9 @@ class PagesController < ApplicationController
   end
 
   def search
-    @grants = Grant.crowdfunding_grants.search params[:query]
+    @grants = Grant.crowdfunding_grants
+                   .search(params[:query])
+                   .includes :recipient, :school
   end
 
   def donors
