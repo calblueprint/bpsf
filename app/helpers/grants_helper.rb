97c6aee1aa3295719c07ef2fbd2b0449b0762904
jaytlin <heyjaylin@gmail.com>
2013-12-07 15:42:34 -0800

    Fix success methods and add (un)success by goal helpers.

--- app/helpers/grants_helper.rb
+++ app/helpers/grants_helper.rb
@@ -5,20 +5,26 @@ module GrantsHelper
   end
 
   def successful
-    @grants
+    @grants = []
     Grant.complete_grants.each do |grant|
       if grant.previous_version.state == 'crowdfunding'
-        @grants.append(grant)
+        @grants << grant
       end
     end
     return @grants
   end
 
   def unsuccessful
-    @grants
+    @grants = []
     Grant.crowdpending_grants.each do |grant|
       if grant.previous_version.state == 'crowdfunding'
-        @grants.append(grant)
+        @grants << grant
+      end
+    end
+    Grant.rejected_grants.each do |grant|
+      flag = grant.previous_version.previous_version
+      if flag && flag.state == 'crowdfunding'
+        @grants << grant
       end
     end
     return @grants
@@ -53,6 +59,24 @@ module GrantsHelper
     @count = []
     Grant::SUBJECTS.each do |subject|
       @grants = Grant.rejected_grants.select { |grant| grant.subject_areas.include? subject }
+      !@grants.nil? ? @count << @grants.length : @count << 0
+    end
+    return @count.to_json
+  end
+
+  def successful_goal
+    @count = []
+    Grant.uniq.pluck(:total_budget).sort_by(&:to_i).each do |goal|
+      @grants = successful.select { |grant| grant.total_budget == goal }
+      !@grants.nil? ? @count << @grants.length : @count << 0
+    end
+    return @count.to_json
+  end
+
+  def unsuccessful_goal
+    @count = []
+    Grant.uniq.pluck(:total_budget).sort_by(&:to_i).each do |goal|
+      @grants = unsuccessful.select { |grant| grant.total_budget == goal }
       !@grants.nil? ? @count << @grants.length : @count << 0
     end
     return @count.to_json
