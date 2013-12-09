d34b211f87e9b20cb12dbfd23cee9cd9f10b3619
varunrau <varunrau@gmail.com>
2013-12-09 02:33:39 -0800

    added phone migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131209084418) do
+ActiveRecord::Schema.define(:version => 20131209103150) do
 
   create_table "admin_profiles", :force => true do |t|
     t.string   "about"
@@ -137,6 +137,8 @@ ActiveRecord::Schema.define(:version => 20131209084418) do
     t.string   "address"
     t.string   "city"
     t.integer  "zipcode"
+    t.string   "work_phone"
+    t.string   "home_phone"
   end
 
   add_index "recipient_profiles", ["school_id"], :name => "index_recipient_profiles_on_school_id"
