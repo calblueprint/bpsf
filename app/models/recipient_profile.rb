1c20ccc2775e24773ecc4afda22129ef675a99ba
varunrau <varunrau@gmail.com>
2013-12-09 02:13:22 -0800

    added phone numbers to recipient profile

--- app/models/recipient_profile.rb
+++ app/models/recipient_profile.rb
@@ -15,11 +15,21 @@
 #
 
 class RecipientProfile < ActiveRecord::Base
-  attr_accessible :about, :image_url, :school_id, :recipient_id, :grade, :subject, :started_teaching, :zipcode, :city, :address
+  include ActionView::Helpers::NumberHelper
+  attr_accessible :about, :image_url, :school_id, :recipient_id, :grade, :subject,
+    :started_teaching, :zipcode, :city, :address, :work_phone, :home_phone
   belongs_to :recipient
+
+  before_save :set_phone_numbers
 
 
   def years_teaching
     Date.today.year - self.started_teaching.year
+  end
+
+  private
+  def set_phone_numbers
+    self.work_phone = number_to_phone(self.work_phone, area_code: true)
+    self.home_phone = number_to_phone(self.home_phone, area_code: true)
   end
 end
