2977ef3ef026e981297660bdde733eae58acdfc0
varunrau <varunrau@gmail.com>
2013-12-09 14:40:25 -0800

    added user profile images

--- app/models/recipient_profile.rb
+++ app/models/recipient_profile.rb
@@ -18,6 +18,8 @@ class RecipientProfile < ActiveRecord::Base
   attr_accessible :about, :image_url, :school_id, :recipient_id, :grade, :subject, :started_teaching
   belongs_to :recipient
 
+  mount_uploader :image_url, ImageUploader
+
   def years_teaching
     Date.today.year - self.started_teaching.year
   end
