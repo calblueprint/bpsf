74f4238b3a7041b9d4732fb31f5b27a2dc2aff30
jaytlin <heyjaylin@gmail.com>
2013-12-09 14:59:46 -0800

    Fix seeds for changed image_url

--- db/seeds.rb
+++ db/seeds.rb
@@ -95,7 +95,7 @@ def make_grants
                                            n_collaborators: n,
                                            collaborators: Faker::Lorem.paragraph,
                                            comments: Faker::Lorem.paragraph,
-                                           image_url: "/assets/default/Art and Music.jpg")
+                                           image_url: File.open(File.join(Rails.root, "app/assets/images/default/Art and Music.jpg")))
     crowdfunding_grants << t2.grants.build(title: "Grant #{n + 4}",
                                            summary: Faker::Lorem.sentence,
                                            subject_areas: ["Mathematics"],
@@ -114,7 +114,7 @@ def make_grants
                                            n_collaborators: n,
                                            collaborators: Faker::Lorem.paragraph,
                                            comments: Faker::Lorem.paragraph,
-                                           image_url: "/assets/default/Science and Math.jpg")
+                                           image_url: File.open(File.join(Rails.root, "app/assets/images/default/Science and Math.jpg")))
   end
   crowdfunding_grants.map do |grant|
     grant.crowdfund
