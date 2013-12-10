1c50d5b22de6d0d507afb3be1543382d769498a1
jaytlin <heyjaylin@gmail.com>
2013-12-10 01:24:10 -0800

    Paginate payment history for user profs.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,6 +5,7 @@ class UserController < ApplicationController
   def show
     @user = User.find params[:id]
     @payments = @user.payments.includes(:crowdfund)
+    @payments = @payments.paginate :page => params[:page], :per_page => 6
     @profile = @user.profile
   end
 
