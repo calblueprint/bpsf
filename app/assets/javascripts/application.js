bc29ad331b6871bca9e3db1e85f7c4838175d2f9
Sam Lau <samlau95@gmail.com>
2014-01-09 11:16:30 -0800

    Applicationjs should require itself

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,6 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
+//= require_self
 //= require_tree .
 //= require jquery_ujs
-
