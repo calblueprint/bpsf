98daece6602537dab9acb89809e8594739fedf2b
Sam Lau <samlau95@gmail.com>
2014-01-30 23:16:03 -0800

    Annotate profile

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: user_profiles
+#
+#  id           :integer          not null, primary key
+#  address      :text
+#  phone        :text
+#  relationship :text
+#  user_id      :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class UserProfile < ActiveRecord::Base
   attr_accessible :address, :phone, :relationship, :user_id
 end
