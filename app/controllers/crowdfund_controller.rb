9f327b213d646fd5efdf99e74ecdc7f34a4a81be
Sam Lau <samlau95@gmail.com>
2014-01-31 13:25:02 -0800

    Update method for crowdfund

--- app/controllers/crowdfund_controller.rb
+++ app/controllers/crowdfund_controller.rb
@@ -1,8 +1,7 @@
 # Controller for the Crowdfund model
 class CrowdfundController < ApplicationController
   def create
-    date = params[:date]
-    time = Time.new date[:year], date[:month], date[:day]
+    time = get_date_from_params
     @grant = Grant.find params[:grant_id]
     @grant.create_crowdfunder deadline: time,
                               goal: params[:goal],
@@ -12,7 +11,24 @@ class CrowdfundController < ApplicationController
     redirect_to @grant
   end
 
+  def update
+    crowdfund = Crowdfund.find params[:crowdfund_id]
+    new_deadline = get_date_from_params
+    if crowdfund.update_attributes deadline: new_deadline
+      flash[:success] = 'Deadline changed!'
+      redirect_to crowdfund.grant
+    else
+      render crowdfund.grant
+    end
+  end
+
   def use_https?
     true
   end
+
+  private
+    def get_date_from_params
+      date = params[:date]
+      Time.new date[:year], date[:month], date[:day]
+    end
 end
