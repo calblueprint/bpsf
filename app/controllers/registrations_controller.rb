7f649b23d53cffaaa2801fa68c6ebbe349e524cd
Wonjunnn <jeong.wonjun@gmail.com>
2014-02-03 19:42:49 -0800

    remove Captcha

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,26 +6,19 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     build_resource sign_up_params
-
-    if verify_recaptcha
-      if resource.save
-        if resource.active_for_authentication?
-          set_flash_message :notice, :signed_up if is_navigational_format?
-          sign_up(resource_name, resource)
-          respond_with resource, location: after_sign_up_path_for(resource)
-        else
-          set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-          expire_session_data_after_sign_in!
-          respond_with resource, location: after_inactive_sign_up_path_for(resource)
-        end
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
       else
-        clean_up_passwords resource
-        respond_with resource
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
     else
-      set_flash_message :error, "invalid_captcha"
-      flash.delete :recaptcha_error
-      redirect_to new_user_session_path
+      clean_up_passwords resource
+      respond_with resource
     end
   end
 
