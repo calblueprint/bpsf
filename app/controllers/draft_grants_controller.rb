4fba005f4f12475b74af56ff72f8fc8b055c17dc
Sam Lau <samlau95@gmail.com>
2014-02-08 00:21:11 -0800

    Remove flash message

--- app/controllers/draft_grants_controller.rb
+++ app/controllers/draft_grants_controller.rb
@@ -44,7 +44,6 @@ class DraftGrantsController < ApplicationController
       flash[:success] = 'Application submitted!'
       redirect_to recipient_dashboard_path
     else
-      flash[:danger] = 'Some fields were not filled in correctly!'
       redirect_to edit_draft_path @draft_grant
     end
   end
