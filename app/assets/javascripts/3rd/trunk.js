c915cc4267bd231576ac1fee8da9e4f508f6628b
vivekraghuram <vv9295@yahoo.com>
2014-02-13 21:29:24 -0800

    changed user dropdown to arrow up style with a slide down effect

--- app/assets/javascripts/3rd/trunk.js
+++ app/assets/javascripts/3rd/trunk.js
@@ -5,26 +5,32 @@ $(function() {
 	var xbox = $('.xbox');
 	var screen = $('.screen');
 	var closeclick = $('.closeclick');
+	var loginmenu = $('.loginmenu')
+	var userdropdown = $('.userdropdown')
 
-	var open = function() {
-							$(items).removeClass('close').addClass('open');
+	var open = function(items, open, close) {
+							$(items).removeClass(close).addClass(open);
 						}
-	var close = function() { 
-							$(items).removeClass('open').addClass('close');
+	var close = function(items, open, close) {
+							$(items).removeClass(open).addClass(close);
 						}
 
 	hp.click(function(){
-		if (hp.hasClass('open')) {$(close)}
-		else {$(open)}
+		if (hp.hasClass('open')) {$(close(items,'open','close'))}
+		else {$(open(items, 'open', 'close'))}
 	});
 	xbox.click(function(){
-		if (hp.hasClass('open')) {$(close)};
+		if (hp.hasClass('open')) {$(close(items,'open','close'))};
 	});
 	screen.click(function(){
-		if (hp.hasClass('open')) {$(close)};
+		if (hp.hasClass('open')) {$(close(items,'open','close'))};
 	});
 	closeclick.click(function(){
-		if (hp.hasClass('open')) {$(close)};
+		if (hp.hasClass('open')) {$(close(items,'open','close'))};
+	});
+	loginmenu.click(function(){
+		if (userdropdown.hasClass('dropper')){$(close(userdropdown,'dropper','close'))}
+		else {$(open(userdropdown,'dropper','close'))}
 	});
 
 });
