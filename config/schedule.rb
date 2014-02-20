1c05705b19113c91335ed919ed5fc729de5b41c9
jaytlin <heyjaylin@gmail.com>
2014-02-20 12:36:16 -0800

    Send 3day email at 1 am instead of random 9:28pm or something..

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, "#{path}/log/cron.log"
 job_type :script, "'#{path}/script/:task' :output"
 
-every 1.day, :at => '9:49 pm' do
+every 1.day, :at => '1:00 am' do
   runner "Grant.grant_ending"
 end
