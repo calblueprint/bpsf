965a1c936a0af3bd8242f9a616c1e3a68ca6a951
Wonjunnn <jeong.wonjun@gmail.com>
2014-03-14 23:05:07 -0700

    Send more information to Stripe. Add billing address to credit card forms

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -20,10 +20,17 @@ customer = {
        processCustomer: function() {
                                var card;
                                card = {
+                                      name: $("#card_name").val(),
                                       number: $("#card_number").val(),
                                       cvc: $("#card_code").val(),
                                       expMonth: $("#card_month").val(),
-                                      expYear: $("#card_year").val()
+                                      expYear: $("#card_year").val(),
+                                      address_line1: $("#address_line1").val(),
+                                      address_line2: $("#address_line2").val(),
+                                      address_city: $("#address_city").val(),
+                                      address_zip: $("#address_zip").val(),
+                                      address_state: $("#address_state").val(),
+                                      address_country: $("#address_country").val()
                                };
                                Stripe.createToken(card, customer.stripeResponseHandler);
                         },
