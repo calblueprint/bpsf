04181ee7de22871440f0b62715dbb9f00ff6dd1b
vivekraghuram <vv9295@yahoo.com>
2014-05-19 21:16:21 -0400

    Remove from comment lines around session stuff

--- app/assets/javascripts/3rd/transitions.js
+++ app/assets/javascripts/3rd/transitions.js
@@ -1,20 +1,15 @@
 $(function() {
 
-
 	var screens = $('.screen, .modalscreen'),
 		isOpened = [];
 
-	var tabGetter = function(e, tabs, type){
+	var tabGetter = function(e, tabs, type, preventDefault){
 		target = $('#' + sessionStorage.getItem('tab'));
-//		var target = $(window.location.hash.replace('tab-',''));
-//		console.log('grabbing tab', window.location.hash);
 		$(tabs).trigger('gumby.set', target.index());
 	};
 
-	var tabSetter = function(e, target, type){
+	var tabSetter = function(e, target, type, preventDefault){
 		sessionStorage.setItem('tab',target.id);
-		//console.log('setting new tab', target.id);
-		//window.location.hash = 'tab-' + target.id.replace('#','');
 	};
 
 	var editClasses = function(objects, addClasses, removeClasses){
@@ -32,7 +27,7 @@ $(function() {
 		};
 	};
 
-	var transitionFunction = function(e, target, type){
+	var transitionFunction = function(e, target, type, preventDefault){
 		var object = [$(target)];
 		//Add object type-specific behavior here
 		if(type == 'modal'){
@@ -56,34 +51,36 @@ $(function() {
 			editClasses(object, openClasses, closeClasses);
 			isOpened.push(object[0]);
 		};
-		e.preventDefault();
-		return false
+		if(preventDefault){
+			e.preventDefault();
+			return false
+		}
 	};
 
 	//Add any modals you want to this list.
-	//[Trigger, Target, Function, Target-type, Event]
+	//[Trigger, Target, Function, Target-type, Event, PreventDefaul?]
 	var bindList = [
-			[document, isOpened, transitionFunction, 'modal', 'keyup'],
-			[document, '.pace', transitionFunction, 'loader change', 'page:change'],
-			[document, '.pace', transitionFunction, 'loader fetch', 'page:fetch'],
-			['#loggedinmenu', '.userdropdown', transitionFunction, 'menu', 'click'],
-			['.xbox, .screen, .modalscreen', isOpened, transitionFunction, 'modal', 'click'],
-			['.toggle_crowdfund', '#crowdfund-form', transitionFunction, 'modal', 'click'],
-			['#terms_conditions', '#terms', transitionFunction, 'modal', 'click'],
-			['.helppanelbutton', '.helppanel', transitionFunction, 'modal', 'click'],
-			['#teacherbutton', '#teacher-about', transitionFunction, 'modal', 'click'],
-			['#aboutbutton', '#about-about', transitionFunction, 'modal', 'click'],
-			['#donorbutton', '#donor-about', transitionFunction, 'modal', 'click'],
-			['#paymentbutton1, #paymentbutton2', '#payment-form', transitionFunction, 'modal', 'click'],
-			['.donate', '#payment-modal', transitionFunction, 'modal', 'click']
+			[document, isOpened, transitionFunction, 'modal', 'keyup', true],
+			[document, '.pace', transitionFunction, 'loader change', 'page:change', true],
+			[document, '.pace', transitionFunction, 'loader fetch', 'page:fetch', true],
+			['#loggedinmenu', '.userdropdown', transitionFunction, 'menu', 'click', true],
+			['.xbox, .screen, .modalscreen', isOpened, transitionFunction, 'modal', 'click', true],
+			['.toggle_crowdfund', '#crowdfund-form', transitionFunction, 'modal', 'click', true],
+			['#terms_conditions', '#terms', transitionFunction, 'modal', 'click', true],
+			['.helppanelbutton', '.helppanel', transitionFunction, 'modal', 'click', true],
+			['#teacherbutton', '#teacher-about', transitionFunction, 'modal', 'click', true],
+			['#aboutbutton', '#about-about', transitionFunction, 'modal', 'click', true],
+			['#donorbutton', '#donor-about', transitionFunction, 'modal', 'click', true],
+			['#paymentbutton1, #paymentbutton2', '#payment-form', transitionFunction, 'modal', 'click', true],
+			['.donate', '#payment-modal', transitionFunction, 'modal', 'click', false]
 		];
 
 	//Bind tab change events if tabs exist on page
 	var tabs = $('.tab-nav');
 	if(tabs.length > 0){
 		bindList.push(
-			[document, tabs, tabGetter, 'tab', 'page:change'],
-			[document, tabs, tabGetter, 'tab', 'ready']	
+			[document, tabs, tabGetter, 'tab', 'page:change', true],
+			[document, tabs, tabGetter, 'tab', 'ready', true]	
 			);
 		var tabChildren = tabs.children();
 	    for(var i = tabChildren.length - 1; i >= 0; i--){
@@ -91,15 +88,15 @@ $(function() {
 	    };
 	};
 
-	var bindFunction = function(trigger, target, func, type, eventType){
+	var bindFunction = function(trigger, target, func, type, eventType, preventDefault){
 		var triggerObject = findObject(trigger);
 		if(triggerObject !== null){
 			triggerObject.on(eventType, function(e){
 				if(typeof target == 'string' || type == 'tab'){
-					func(e, target, type);
+					func(e, target, type, preventDefault);
 				} else if(e.keyCode == 27 || e.keyCode == undefined && target == isOpened) {
 					for (var i = target.length - 1; i >= 0; i--) {
-						func(e, target[i], type);
+						func(e, target[i], type, preventDefault);
 						target.pop();
 					};
 				};
@@ -107,16 +104,17 @@ $(function() {
 		};
 	};
 
-	/*Takes a list of triggers, targets, functions, target types and events 
-	and binds the function to the target on that event*/
+	/*Takes a list of triggers, targets, functions, target types, events and
+	whether to preventDefault and binds the function to the target on that event*/
 	var bindAll = function(bindList){
 		for (var i = bindList.length - 1; i >= 0; i--) {
 			var trigger = bindList[i][0],
 				target  = bindList[i][1],
 				func = bindList[i][2],
 				type = bindList[i][3],
-				eventType = bindList[i][4];
-			bindFunction(trigger, target, func, type, eventType);
+				eventType = bindList[i][4],
+				preventDefault = bindList[i][5];
+			bindFunction(trigger, target, func, type, eventType, preventDefault);
 		};
 	};
 
