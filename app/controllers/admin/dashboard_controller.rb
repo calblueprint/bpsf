b63b51ae766109dd3d3292f516e78d6cc9735148
jaytlin <heyjaylin@gmail.com>
2014-06-01 02:21:10 -0700

    Load tabs separately.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -7,24 +7,6 @@ class Admin::DashboardController < ApplicationController
     if !current_user.approved
       raise CanCan::AccessDenied.new
     end
-    @grants = (Grant.all-DraftGrant.all).sort_by(&:order_status).paginate :page => params[:page], :per_page => 6
-    @donors = User.donors
-    donated = params[:donated]
-    if donated && donated == 'Donated'
-      @donors = User.donors.select {|user| user.payments.length > 0}
-    elsif donated && donated == 'Have Not Donated'
-      @donors = User.donors.select {|user| user.payments.length == 0}
-    end
-    @donors = @donors.paginate :page => params[:page], :per_page => 6
-
-    @recipients = Recipient.all
-    school = params[:school]
-    if school && school != 'All'
-      schoolId = School.find_by_name(school).id
-      @recipients = Recipient.select {|recip| recip.profile.school_id == schoolId }
-    end
-    @recipients = @recipients.paginate :page => params[:page], :per_page => 6
-
     @pending_users = User.where approved: false
     @pending_users = @pending_users.paginate :page => params[:page], :per_page => 6
   end
@@ -34,6 +16,33 @@ class Admin::DashboardController < ApplicationController
     @grant.send params[:state]
     respond_to do |format|
       format.html { redirect_to admin_dashboard_path }
+      format.js
+    end
+  end
+
+  def load_grants
+    @grants = (Grant.all-DraftGrant.all).sort_by(&:order_status).paginate :page => params[:page], :per_page => 6
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def load_recipients
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def load_donors
+    @donors = User.donors
+    donated = params[:donated]
+    if donated && donated == 'Donated'
+      @donors = User.donors.select {|user| user.payments.length > 0}
+    elsif donated && donated == 'Have Not Donated'
+      @donors = User.donors.select {|user| user.payments.length == 0}
+    end
+    @donors = @donors.paginate :page => params[:page], :per_page => 6
+    respond_to do |format|
       format.js
     end
   end
