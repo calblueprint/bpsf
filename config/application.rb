225a975d6f6fdea442d29a294b555116636b78e5
Sam Lau <samlau95@gmail.com>
2014-06-12 22:29:13 -0700

    Load concerns folder in controllers

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module BPSF
     # Autoreload lib/ and concerns folder
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.autoload_paths += %W(#{config.root}/app/models/concerns)
+    config.autoload_paths += %W(#{config.root}/app/controllers/concerns)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
