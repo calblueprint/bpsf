e075a2d77b53c11489d4c63d0ef5675d91d7edf1
vivekraghuram <vv9295@yahoo.com>
2014-07-24 17:01:23 -0700

    Add money to seed, setup basic controller skeletons, create fundingbar function

--- app/assets/javascripts/3rd/bp-controller.js
+++ app/assets/javascripts/3rd/bp-controller.js
@@ -1,32 +1,34 @@
-var AppController = function(namespace){
+var AppController = function(documentObject){
 	var me = this;
-	me.namespace = namespace || document;
+	me.documentObject = documentObject || document;
 	me.activeControllers = [];
+	me.wind = window;
 
 	me.init = function(){
 		console.log('Initializing AppController...');
 		me.objectCreatePolyfill();
 
 		me.findController();
-		if(me.controllerNamespaces){
+		if(me.controllerdocumentObjects){
 			me.activateControllers();
 		}
 	}
 
 	me.findController = function(){
-		me.controllerNamespaces = me.namespace.querySelectorAll('[data-bp-controller]');
+		me.controllerdocumentObjects = me.documentObject.querySelectorAll('[data-bp-controller]');
 	}
 
 	me.activateControllers = function(){
-		for (var i = me.controllerNamespaces.length - 1; i >= 0; i--) {
-			var namespace = me.controllerNamespaces[i],
-				controller = namespace.getAttribute('data-bp-controller');
+		for (var i = me.controllerdocumentObjects.length - 1; i >= 0; i--) {
+			var documentObject = me.controllerdocumentObjects[i],
+				controller = documentObject.getAttribute('data-bp-controller');
 			if(typeof(controller) == 'string'){
 				try{
-					newController = window[controller](namespace);
-					me.activateControllers.push(newController);
+					controllerConstructor = window[controller];
+					newController = new controllerConstructor(documentObject);
+					me.activeControllers.push(newController);
 				} catch(err){
-					console.log('Error: Improper controller name.' + controller + ' does not exist.');
+					console.log('Error: Improper controller name. ' + controller + ' does not exist.');
 					console.log(err.message);
 				}
 			} else {
@@ -55,24 +57,99 @@ var AppController = function(namespace){
 		    })();
 		}
 	}
+
+	me.navigationBar = function(){}
+
+	me.turbolinkHook = function(){}
+
+	me.extendPageLength = function(){}
+
+	me.setActiveElements = function(){}
+
+	me.getActiveElements = function(){}
+
+	me.clearActiveElements = function(){}
+
+	me.initDataStore = function(){}
+
+	me.setDataStore = function(){}
+
+	me.getDataStore = function(){}
+
+	me.manageTabs = function(){}
+
+	me.setActiveTab = function(){}
+
+	me.getActiveTab = function(){}
 }
 
-var HomeController = function(namespace){
+var HomeController = function(documentObject){
 	var me = this;
-	AppController.call(me, namespace);
+	AppController.call(me, documentObject);
+	me.documentObject = documentObject || document;
 
 	me.init = function(){
-		console.log('Initializing HomeController');
-		me.createAlert();
+		me.fundingProgress();
 	}
 
-	me.createAlert = function(){
-		alert('HomeController working' + this);
+	//This needs to be wired to fire when more grants are loaded from scrolling
+	me.fundingProgress = function(){
+		var fundingBars = me.documentObject.querySelectorAll('.funding-bar');
+		for (var i = fundingBars.length - 1; i >= 0; i--) {
+			var currentFunding = fundingBars[i].getAttribute('data-current'),
+				goalFunding = fundingBars[i].getAttribute('data-goal'),
+				fundingBarWidth = currentFunding/goalFunding * 100;
+			if (fundingBarWidth > 0 && fundingBarWidth < 10){
+				fundingBarWidth = 10;
+			} else if(fundingBarWidth > 100){
+				fundingBarWidth = 100;
+			}
+			fundingBars[i].style.width = String(fundingBarWidth) + '%';
+		};
 	}
+
+	me.truncateText = function(){}
+
+	me.infiniteScroll = function(){}
+
+	me.superSlider = function(){}
 }
 
 HomeController.prototype = Object.create(AppController.prototype);
 HomeController.prototype.constructor = HomeController;
 
 
+var EditGrantController = function(documentObject){
+	var me = this;
+	AppController.call(me, documentObject);
+	me.documentObject = documentObject || document;
 
+	me.init = function(){}
+}
+
+EditGrantController.prototype = Object.create(AppController.prototype);
+EditGrantController.prototype.constructor = EditGrantController;
+
+
+var ShowGrantController = function(documentObject){
+	var me = this;
+	AppController.call(me, documentObject);
+	me.documentObject = documentObject || document;
+
+	me.init = function(){}
+}
+
+ShowGrantController.prototype = Object.create(AppController.prototype);
+ShowGrantController.prototype.constructor = ShowGrantController;
+
+
+var AdminDashboardController = function(documentObject){
+	var me = this;
+	AppController.call(me, documentObject);
+	me.documentObject = documentObject || document;
+
+	me.init = function(){}
+}
+
+AdminDashboardController.prototype = Object.create(AppController.prototype);
+AdminDashboardController.prototype.constructor = AdminDashboardController;
