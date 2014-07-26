270bd1ab32c0d857a7c5377fd3700c3cfd11cd01
vivekraghuram <vv9295@yahoo.com>
2014-07-26 16:24:52 -0700

    Reorganize appcontroller more and add modalbinding function/syntax

--- app/assets/javascripts/controllers/AppController.js
+++ app/assets/javascripts/controllers/AppController.js
@@ -1,25 +1,12 @@
-var AppController = function(documentObject){
-	var me = this;
-	me.documentObject = documentObject || document;
-	me.activeControllers = [];
+(function($){
 
-	me.init = function(){
-		console.log('Initializing AppController...');
-		me.objectCreatePolyfill();
-		me.checkBrowserVersion();
-
-		me.findController();
-		if(me.controllerdocumentObjects){
-			me.activateControllers();
-		}
-	}
-
-	me.checkBrowserVersion = function(){
-		if('querySelector' in document &&
+	function ifOldieAlert(){
+		console.log('Checking if oldie browser...')
+		if(!('querySelector' in document &&
 			'localStorage' in window &&
-			'addEventListener' in window){
-			return true;
-		} else {
+			'addEventListener' in window &&
+			'create' in Object))
+		{
 			var response = confirm('This site is designed for modern web browsers \
 									and may offer a subpar experience in older \
 									versions. We kindly suggest you update your web browser \
@@ -28,35 +15,12 @@ var AppController = function(documentObject){
 			if (response){
 				window.location.href = 'http://whatbrowser.org/';
 			} else {
-				return true;
+				objectCreatePolyfill();
 			}
 		}
 	}
 
-	me.findController = function(){
-		me.controllerdocumentObjects = me.documentObject.querySelectorAll('[data-bp-controller]');
-	}
-
-	me.activateControllers = function(){
-		for (var i = me.controllerdocumentObjects.length - 1; i >= 0; i--) {
-			var documentObject = me.controllerdocumentObjects[i],
-				controller = documentObject.getAttribute('data-bp-controller');
-			if(typeof(controller) == 'string'){
-				try{
-					controllerConstructor = window[controller];
-					newController = new controllerConstructor(documentObject);
-					me.activeControllers.push(newController);
-				} catch(err){
-					console.log('Error: Improper controller name. ' + controller + ' does not exist.');
-					console.log(err.message);
-				}
-			} else {
-				console.log('Error: Improper controller declaration.' + controller + ' is not a string.')
-			}
-		}
-	}
-
-	me.objectCreatePolyfill = function(){
+	function objectCreatePolyfill(){
 		if (typeof Object.create != 'function') {
 		    (function () {
 		        var F = function () {};
@@ -77,98 +41,147 @@ var AppController = function(documentObject){
 		}
 	}
 
-	me.navigationBar = function(){}
+	ifOldieAlert();
 
-	me.turbolinkHook = function(){}
 
-	me.extendPageLength = function(){}
 
-	me.setActiveElements = function(){}
+	function AppController(documentObject){
+		var me = this;
+		me.documentObject = documentObject || document;
+		me.activeControllers = [];
 
-	me.getActiveElements = function(){}
+		me.init = function(){
+			console.log('Initializing AppController...');
 
-	me.clearActiveElements = function(){}
+			me.findControllers();
+			if(me.controllerdocumentObjects){
+				me.activateControllers();
+			}
+		}
 
-	me.initDataStore = function(){}
+		me.initOnPageChange = function(){}
 
-	me.setDataStore = function(){}
+		me.findControllers = function(){
+			me.controllerdocumentObjects = me.documentObject.querySelectorAll('[data-bp-controller]');
+		}
 
-	me.getDataStore = function(){}
+		me.activateControllers = function(){
+			for (var i = me.controllerdocumentObjects.length - 1; i >= 0; i--) {
+				var documentObject = me.controllerdocumentObjects[i],
+					controller = documentObject.getAttribute('data-bp-controller');
+				if(typeof(controller) == 'string'){
+					try{
+						controllerConstructor = window[controller];
+						newController = new controllerConstructor(documentObject);
+						me.setActiveController(newController);
+						newController.init();
+					} catch(err){
+						console.log('Error: Improper controller name. ' + controller + ' does not exist.');
+						console.log(err.message);
+					}
+				} else {
+					console.log('Error: Improper controller declaration.' + controller + ' is not a string.')
+				}
+			}
+		}
 
-	me.manageTabs = function(){}
+		me.setActiveController = function(controller){
+			me.activeControllers.push(controller);
+		}
 
-	me.setActiveTab = function(){}
+		me.getActiveControllers = function(){
+			return me.activateControllers;
+		}
 
-	me.getActiveTab = function(){}
-}
-
-var HomeController = function(documentObject){
-	var me = this;
-	AppController.call(me, documentObject);
-	me.documentObject = documentObject || document;
-
-	me.init = function(){
-		me.fundingProgress();
+		me.deactivateControllers = function(){
+			var activeControllers = me.getActiveControllers();
+			for (var i = activeControllers.length - 1; i >= 0; i--) {
+				activeControllers[i].removeEvents();
+				activeControllers[i].clearData();
+				delete activeControllers[i];
+			};
+		}
 	}
 
-	//This needs to be wired to fire when more grants are loaded from scrolling
-	me.fundingProgress = function(){
-		var fundingBars = me.documentObject.querySelectorAll('.funding-bar');
-		for (var i = fundingBars.length - 1; i >= 0; i--) {
-			var currentFunding = fundingBars[i].getAttribute('data-current'),
-				goalFunding = fundingBars[i].getAttribute('data-goal'),
-				fundingBarWidth = currentFunding/goalFunding * 100;
-			if (fundingBarWidth > 0 && fundingBarWidth < 10){
-				fundingBarWidth = 10;
-			} else if(fundingBarWidth > 100){
-				fundingBarWidth = 100;
-			}
-			fundingBars[i].style.width = String(fundingBarWidth) + '%';
+	window['AppController'] = AppController;
+
+	AppController.prototype.modalBind = function(){
+		var me = this,
+			modalButtons = me.documentObject.querySelectorAll('[data-bp-modal]');
+		for (var i = modalButtons.length - 1; i >= 0; i--) {
+			(function(){
+				var targetModal = $(modalButtons[i].getAttribute('data-bp-modal')),
+					isPreserve = modalButtons[i].getAttribute('data-bp-preserve'),
+					triggerButton = $(modalButtons[i]),
+					closeButton = targetModal.find('.xbox'),
+					modalScreen = targetModal.find('.modalscreen');
+				
+				if(targetModal == []){
+					return console.log('Improper modal declaration at ' + modalButtons[i]);
+				}
+
+				$(triggerButton).on('click', function(e){
+					targetModal.addClass('active');
+					e.preventDefault();
+					return false;
+				});
+
+				$(closeButton).on('click', function(e){
+					targetModal.removeClass('active');
+					e.preventDefault();
+					return false;
+				});
+
+				$(modalScreen).on('click', function(e){
+					targetModal.removeClass('active');
+					e.preventDefault();
+					return false;
+				})
+
+				if(!isPreserve){
+					me.setTempEvent(triggerButton, closeButton, modalScreen);
+				} else {
+					me.setPreservedEvent(triggerButton, closeButton, modalscreen);
+				}
+
+			})();
 		};
 	}
 
-	me.truncateText = function(){}
+	AppController.prototype.removeEvents = function(){}
 
-	me.infiniteScroll = function(){}
+	AppController.prototype.clearData = function(){}
 
-	me.superSlider = function(){}
-}
+	AppController.prototype.setTempEvent = function(){}
 
-HomeController.prototype = Object.create(AppController.prototype);
-HomeController.prototype.constructor = HomeController;
+	AppController.prototype.setPreservedEvent = function(){}
 
+	AppController.prototype.getTempEvent = function(){}
 
-var EditGrantController = function(documentObject){
-	var me = this;
-	AppController.call(me, documentObject);
-	me.documentObject = documentObject || document;
+	AppController.prototype.getPreservedEvent = function(){}
 
-	me.init = function(){}
-}
+	AppController.prototype.navigationBar = function(){}
 
-EditGrantController.prototype = Object.create(AppController.prototype);
-EditGrantController.prototype.constructor = EditGrantController;
+	AppController.prototype.turbolinkHook = function(){}
 
+	AppController.prototype.extendPageLength = function(){}
 
-var ShowGrantController = function(documentObject){
-	var me = this;
-	AppController.call(me, documentObject);
-	me.documentObject = documentObject || document;
+	AppController.prototype.setActiveElements = function(){}
 
-	me.init = function(){}
-}
+	AppController.prototype.getActiveElements = function(){}
 
-ShowGrantController.prototype = Object.create(AppController.prototype);
-ShowGrantController.prototype.constructor = ShowGrantController;
+	AppController.prototype.clearActiveElements = function(){}
 
+	AppController.prototype.initDataStore = function(){}
 
-var AdminDashboardController = function(documentObject){
-	var me = this;
-	AppController.call(me, documentObject);
-	me.documentObject = documentObject || document;
+	AppController.prototype.setDataStore = function(){}
 
-	me.init = function(){}
-}
+	AppController.prototype.getDataStore = function(){}
 
-AdminDashboardController.prototype = Object.create(AppController.prototype);
-AdminDashboardController.prototype.constructor = AdminDashboardController;
+	AppController.prototype.manageTabs = function(){}
+
+	AppController.prototype.setActiveTab = function(){}
+
+	AppController.prototype.getActiveTab = function(){}
+
+})(jQuery);
