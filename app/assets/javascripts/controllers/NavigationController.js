2991babb7e2e51d5d171bd8d9d26fd806708aa8b
vivekraghuram <vv9295@yahoo.com>
2014-07-29 08:21:26 -0700

    Add dropdown bind method

--- app/assets/javascripts/controllers/NavigationController.js
+++ app/assets/javascripts/controllers/NavigationController.js
@@ -4,11 +4,47 @@ var NavigationController = function(documentObject){
 	me.documentObject = documentObject || document;
 
 	me.init = function(){
-		
+		me.modalBind();
+		me.dropdownBind();
 	}
-
 	
 }
 
 NavigationController.prototype = Object.create(AppController.prototype);
 NavigationController.prototype.constructor = NavigationController;
+
+
+NavigationController.prototype.dropDownBind = function(){
+		var me = this,
+			dropDownButtons = me.documentObject.querySelectorAll('[data-bp-dropdown]');
+		for (var i = dropDownButtons.length - 1; i >= 0; i--) {
+			(function(){
+				var targetDropDown = $(dropDownButtons[i].getAttribute('data-bp-dropdown')),
+					isPreserve = dropDownButtons[i].getAttribute('data-bp-preserve'),
+					triggerButton = $(dropDownButtons[i]);
+				
+				if(targetDropDown == []){
+					return console.log('Improper dropdown declaration at ' + dropDownButtons[i]);
+				}
+
+				$(triggerButton).on('click', function(e){
+					if(targetDropDown.hasClass('active')){
+						targetDropDown.addClass('active');
+						triggerButton.addClass('active');
+					} else {
+						targetDropDown.removeClass('active');
+						triggerButton.removeClass('active');
+					}
+					e.preventDefault();
+					return false;
+				});
+				
+				if(!isPreserve){
+					me.setTempEvent(triggerButton, closeButton, dropDownScreen);
+				} else {
+					me.setPreservedEvent(triggerButton, closeButton, dropDownscreen);
+				}
+
+			})();
+		};
+	}
