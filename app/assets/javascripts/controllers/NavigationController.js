9ef644536bc0a1e467d57ceb6bd1a18b32d953dc
vivekraghuram <vv9295@yahoo.com>
2014-07-29 22:33:59 -0700

    Add navigation binding functions element activation functions

--- app/assets/javascripts/controllers/NavigationController.js
+++ app/assets/javascripts/controllers/NavigationController.js
@@ -15,36 +15,52 @@ NavigationController.prototype.constructor = NavigationController;
 
 
 NavigationController.prototype.dropDownBind = function(){
-		var me = this,
-			dropDownButtons = me.documentObject.querySelectorAll('[data-bp-dropdown]');
-		for (var i = dropDownButtons.length - 1; i >= 0; i--) {
-			(function(){
-				var targetDropDown = $(dropDownButtons[i].getAttribute('data-bp-dropdown')),
-					isPreserve = dropDownButtons[i].getAttribute('data-bp-preserve'),
-					triggerButton = $(dropDownButtons[i]);
-				
-				if(targetDropDown == []){
-					return console.log('Improper dropdown declaration at ' + dropDownButtons[i]);
-				}
+	var me = this,
+		dropDownButtons = me.documentObject.querySelectorAll('[data-bp-dropdown]');
+	for (var i = dropDownButtons.length - 1; i >= 0; i--) {
+		(function(){
+			var targetDropDown = $(dropDownButtons[i].getAttribute('data-bp-dropdown')),
+				triggerButton = $(dropDownButtons[i]);
+			
+			if(targetDropDown == []){
+				return console.log('Improper dropdown declaration at ' + dropDownButtons[i]);
+			}
 
-				$(triggerButton).on('click', function(e){
-					if(targetDropDown.hasClass('active')){
-						targetDropDown.addClass('active');
-						triggerButton.addClass('active');
-					} else {
-						targetDropDown.removeClass('active');
-						triggerButton.removeClass('active');
-					}
-					e.preventDefault();
-					return false;
-				});
-				
-				if(!isPreserve){
-					me.setTempEvent(triggerButton, closeButton, dropDownScreen);
-				} else {
-					me.setPreservedEvent(triggerButton, closeButton, dropDownscreen);
-				}
+			$(triggerButton).on('click', function(e){
+				me.flipElementStates(targetDropDown, triggerButton);
+				e.preventDefault();
+				return false;
+			});
+			
+		})();
+	};
+}
 
-			})();
-		};
-	}
+NavigationController.prototype.mobileNavBind = function(){
+	var me = this,
+		mobileNavButtons = me.documentObject.querySelectorAll('[data-bp-mobilenav]');
+	for (var i = mobileNavButtons.length - 1; i >= 0; i--) {
+		(function(){
+			var targetNavigation = $(mobileNavButtons[i].getAttribute('data-bp-mobilenav')),
+				modalScreen = targetNavigation.find('.modalscreen'),
+				triggerButton = $(mobileNavButtons[i]);
+			
+			if(targetNavigation == []){
+				return console.log('Improper mobilenav declaration at ' + mobileNavButtons[i]);
+			}
+
+			$(triggerButton).on('click', function(e){
+				me.activateElements(targetNavigation, modalScreen);
+				e.preventDefault();
+				return false;
+			});
+
+			$(modalScreen).on('click', function(e){
+				me.deactivateElements(targetNavigation, modalScreen);
+				e.preventDefault();
+				return false;
+			});
+
+		})();
+	};
+}
