f36b534d74fe5d45ece01566affa550248e530ab
vivekraghuram <vv9295@yahoo.com>
2014-08-03 14:10:05 -0700

    Properly integrate homecontroller js

--- app/assets/javascripts/controllers/HomeController.js
+++ app/assets/javascripts/controllers/HomeController.js
@@ -4,6 +4,8 @@ var HomeController = function(documentObject){
 	me.documentObject = documentObject || document;
 
 	me.init = function(){
+		me.superSlider();
+		me.infiniteScroll();
 		me.fundingProgress();
 		me.turbolinkBind();
 	}
@@ -26,9 +28,39 @@ var HomeController = function(documentObject){
 
 	me.truncateText = function(){}
 
-	me.infiniteScroll = function(){}
+	me.infiniteScroll = function(){
+		$(window).scroll(function(){
+	    var url = $('.pagination .next_page').attr('href');
+	    if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 150) {
+	        $('.pagination').text('Fetching more grants...')
+	        $.getScript(url);
+	    }
+	  });
+	}
 
-	me.superSlider = function(){}
+	me.superSlider = function(){
+		var me = this;
+	    $('.home-slider').superslides({
+			play: 8000,
+			pagination: false,
+			hashchange: false,
+			inherit_width_from: '.home-slider-container',
+			inherit_height_from: '.home-slider-container'
+	    });
+
+		var nextArrow = me.documentObject.getElementsByClassName('arrow-box-right'),
+			prevArrow = me.documentObject.getElementsByClassName('arrow-box-left');
+
+		console.log(nextArrow, 'is next arrow');
+		
+		$(nextArrow).on('click', function(){
+			$('.home-slider').superslides('animate','next');
+		});
+		
+		$(prevArrow).on('click', function(){
+			$('.home-slider').superslides('animate','prev');
+		});
+	}
 }
 
 HomeController.prototype = Object.create(AppController.prototype);
