f858892c55f3c83d47f94b9418bd4deb8d2dd169
varunrau <varunrau@gmail.com>
2014-08-03 23:50:10 -0700

    removed console logging

--- app/assets/javascripts/admin/dashboard.js
+++ app/assets/javascripts/admin/dashboard.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $(".chosenselect").chosen({width:'90%'});
   $("input[name=donors]").attr("disabled", true);
   $("#donor-selection").change(function() {
-  	console.log("donor-selection changed");
   	var selection = $("#donor-selection")[0].selectedIndex;
   	if (selection == 0) {
   		// Placeholder
