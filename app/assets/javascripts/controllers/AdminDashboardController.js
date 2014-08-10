7a819a665a558116596b3b88abe2d795a31c16e0
vivekraghuram <vv9295@yahoo.com>
2014-08-10 15:53:55 -0700

    Merge csv and phrasing

--- app/assets/javascripts/controllers/AdminDashboardController.js
+++ app/assets/javascripts/controllers/AdminDashboardController.js
@@ -5,6 +5,18 @@ var AdminDashboardController = function(documentObject){
 
 	me.init = function(){
 		me.manageTabs();
+
+		$(".chosenselect").chosen({width:'90%'});
+		$("input[name=donors]").attr("disabled", true).addClass('disabled');
+		$("#donor-selection").change(function() {
+			var selection = $("#donor-selection")[0].selectedIndex;
+			if (selection == 0) {
+				// Placeholder
+				$("input[name=donors]").attr("disabled", true);
+			} else {
+				$("input[name=donors]").attr("disabled", false);
+			}
+		});
 	}
 }
 
