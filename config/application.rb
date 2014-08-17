599acb2713a0242d6daf32586fa031ab2d8df0a3
Sam Lau <samlau95@gmail.com>
2014-08-17 08:22:38 -0700

    Don't need to initialize with local_env anymore

--- config/application.rb
+++ config/application.rb
@@ -67,13 +67,6 @@ module BPSF
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    # Config the local_env.yml
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
-    end
     config.assets.initialize_on_precompile = false
   end
 end
