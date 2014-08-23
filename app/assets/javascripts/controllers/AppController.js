482c5ed177dc5d944ea7d654a7a6d559ee573f6e
Vivek Raghuram <vivek@Viveks-MacBook-Pro.local>
2014-08-22 23:17:53 -0700

    Create checkbox function

--- app/assets/javascripts/controllers/AppController.js
+++ app/assets/javascripts/controllers/AppController.js
@@ -59,7 +59,7 @@
 			}
 			me.bindLoader();
 			me.bindEscElements();
-			
+
 
 			$(document).one('page:fetch',function(){
 				me.deactivateControllers();
@@ -335,6 +335,29 @@
 		} else {
 			return false;
 		}
+	}
+
+	AppController.prototype.checkboxBind = function(){
+		var me = this,
+			checkboxes = me.documentObject.querySelectorAll('.checkbox');
+
+		for (var i = checkboxes.length - 1; i >= 0; i--) {
+			(function(){
+				var checkbox = checkboxes[i]
+				$(checkbox).on('click', function(e){
+					var inputEl = checkbox.querySelector('input[type="checkbox"]');
+					if(inputEl.checked){
+						me.deactivateElements(checkbox);
+					} else {
+						me.activateElements(checkbox);
+					}
+					inputEl.checked = !inputEl.checked;
+					e.preventDefault();
+					return false;
+				});
+			})();
+		};
+
 	}
 
 })(jQuery);
