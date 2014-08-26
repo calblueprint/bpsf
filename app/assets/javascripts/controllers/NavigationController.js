99de0c49a942532d5e91a66b3112eed31eec148f
vivekraghuram <VV9295@YAHOO.COM>
2014-08-26 16:52:39 -0700

    Add back button support to mobile menus

--- app/assets/javascripts/controllers/NavigationController.js
+++ app/assets/javascripts/controllers/NavigationController.js
@@ -21,6 +21,7 @@ NavigationController.prototype.dropDownBind = function(){
 	for (var i = dropDownButtons.length - 1; i >= 0; i--) {
 		(function(){
 			var targetDropDown = me.documentObject.querySelector(dropDownButtons[i].getAttribute('data-bp-dropdown')),
+				backButton = targetDropDown.querySelector('.go-back'),
 				triggerButton = dropDownButtons[i];
 			
 			if(targetDropDown == []){
@@ -29,6 +30,12 @@ NavigationController.prototype.dropDownBind = function(){
 
 			$(triggerButton).on('click', function(e){
 				me.flipElementStates(targetDropDown, triggerButton);
+				e.preventDefault();
+				return false;
+			});
+
+			$(backButton).on('click', function(e){
+				me.deactivateElements(targetDropDown, triggerButton);
 				e.preventDefault();
 				return false;
 			});
@@ -57,7 +64,7 @@ NavigationController.prototype.mobileNavBind = function(){
 			});
 
 			$(modalScreen).on('click', function(e){
-				me.deactivateElements(targetNavigation, modalScreen);
+				me.clearActiveElements();
 				e.preventDefault();
 				return false;
 			});
