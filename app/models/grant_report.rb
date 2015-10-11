fd48a130de9f34132555c009f66e6504645b3418
wonjun <jeong.wonjun@gmail.com>
2015-10-11 01:22:54 -0700

    Finish implementing admin_dash_v2 using datagrid

--- app/models/grant_report.rb
+++ app/models/grant_report.rb
@@ -1,5 +1,7 @@
 class GrantReport
   include Datagrid
+  include Rails.application.routes.url_helpers
+
 
   #
   # Scope
@@ -13,15 +15,28 @@ class GrantReport
   # Filters
   #
 
-  filter(:condition, :dynamic, :header => "Customizeable Conditions")
+  filter(:condition, :dynamic, :header => "Custom Filter:")
+  filter(:updated_at, :date, header: "Updated Between:", :range => true, :default => proc { [3.month.ago.to_date, Date.today]})
 
   #
   # Columns
   #
 
-  column(:id, :mandatory => true)
-  column(:title, :mandatory => true)
-  column(:status, :mandatory => true)
-  column(:updated_at, :mandatory => true)
 
+  column(:teacher_name, mandatory: true)
+  column(:title, mandatory: true) do |grant|
+      format(grant.title) do |value|
+          link_to value, grant_path(grant)
+      end
+  end
+  column(:status, order: :state , mandatory: true)
+  column(:current_funds, header: "Current Funds", mandatory: true) do |grant|
+      grant.current_funds
+  end
+  column(:goal, header: "Goal", mandatory: true) do |grant|
+      grant.total_budget or 0
+  end
+  column(:updated_at, mandatory: true) do |grant|
+      grant.updated_at.to_formatted_s(:long)
+  end
 end
